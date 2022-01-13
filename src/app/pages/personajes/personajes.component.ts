70935905c9b79c531c49b8342181e02046d1493b
Denis Gil <denisgilsil.21@gmail.com>
2022-01-13 17:28:13 -0600

    Tipado de las respuestas http y optimizacion de codigo

--- src/app/pages/personajes/personajes.component.ts
+++ src/app/pages/personajes/personajes.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { PersonajesService } from '../../services/personajes.service';
+import { Personaje, Info } from '../../interfaces/personaje.interface';
 
 @Component({
   selector: 'app-personajes',
@@ -10,54 +11,49 @@ export class PersonajesComponent implements OnInit, OnChanges {
 
   @Input() busqueda: string = '';
 
-  public personajes: any = [];
-  public info:any;
-  public current_page:number = 1;
+  public personajes: Personaje[] = [];
+  public info!: Info;
+  public current_page: number = 1;
 
-  public cargando:boolean = true;
-  
+  public cargando: boolean = true;
+
   constructor(private personajesService: PersonajesService) { }
 
   ngOnInit(): void {
-    this.obtenerPersonajes();
+    this.obtenerPersonajes(this.busqueda,this.current_page);
   }
 
   ngOnChanges(): void {
-      this.obtenerPersonajes();
+    this.obtenerPersonajes(this.busqueda,this.current_page);
+
   }
 
-  
 
-  obtenerPersonajes(){
+
+  obtenerPersonajes(busqueda:string, pagina: number): void {
     this.cargando = true;
-    this.personajesService.getPersonajes(this.busqueda)
-    .subscribe((resp:any) => {
-      this.personajes = resp.results;
-      this.info = resp.info;
-      this.cargando = false;
-      
-    },(err) => {      
-      this.cargando = false;
-      this.personajes = [];
-    });
+    this.personajesService.getPersonajes(busqueda, pagina)
+      .subscribe(resp => {
+        this.personajes = resp.results;
+        this.info = resp.info;
+        this.cargando = false;
+
+      }, (err) => {
+        this.cargando = false;
+        this.personajes = [];
+      });
   }
 
-  cambiarPagina(cambio:number,especifico=false){
+  cambiarPagina(cambio: number, especifico = false): void {
     if (!especifico) {
-      this.current_page +=cambio;
-    }else{
+      this.current_page += cambio;
+    } else {
       this.current_page = cambio;
     }
-    this.cargando = true;
-    this.personajesService.getPersonajes(this.busqueda,this.current_page)
-    .subscribe((resp:any) => {
-      this.personajes = resp.results;
-      this.info = resp.info;    
-      this.cargando = false;
-    })
+    this.obtenerPersonajes(this.busqueda,this.current_page);
   }
 
-  pagesNumber(){    
+  pagesNumber(): number[] {
     let desde = this.current_page - 2;
     if (desde < 1) {
       desde = 1;
@@ -67,12 +63,12 @@ export class PersonajesComponent implements OnInit, OnChanges {
     if (hasta >= this.info.pages) {
       hasta = this.info.pages;
     }
-    let pageArray:any[]=[];
+    let pageArray: any[] = [];
     while (desde <= hasta) {
       pageArray.push(desde);
-      desde ++;
+      desde++;
     }
-    
+
     return pageArray;
   }
 
--- src/app/services/personajes.service.ts
+++ src/app/services/personajes.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { PersonajesData } from '../interfaces/personaje.interface';
+
+const url = 'https://rickandmortyapi.com/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonajesService {
 
-  public url: string = 'https://rickandmortyapi.com/api'
-
   constructor(private http: HttpClient) { }
 
-  getPersonajes(filtro:string = '', page:number = 1){
+  getPersonajes(filtro:string = '', page:number = 1):Observable<PersonajesData>{
 
     let params = new HttpParams();
     params = params.set('page',page);
@@ -19,7 +21,7 @@ export class PersonajesService {
       params = params.set('name', filtro);
     }
 
-    return this.http.get(`${this.url}/character`, {params})
+    return this.http.get<PersonajesData>(`${url}/character`, {params})
   }
 
 
